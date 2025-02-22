c48640a9ca2d182cf69b3b6bd71af3546c041996
syma sultana <symasultana2002@gmail.com>
2025-02-22 20:48:18 +0600

    adding header

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between items-center p-4 mx-4 border-b-2'>
               <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+              <img src={profile} alt="" />
         </div>
     );
 };
