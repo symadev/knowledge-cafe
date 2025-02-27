5350d74046718277d6d31d263406ab593d844b2b
syma sultana <symasultana2002@gmail.com>
2025-02-27 17:42:52 +0600

    added the hashtags

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,12 +1,36 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-  const {title, cover} = blog;
+  const {title, cover,author, author_img, posted_date,reading_time ,hashtags} = blog;
   console.log(blog);
     return (
         <div>
+          
           <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+
+              <div className='flex gap-4'>
+                <img className='w-14' src={author_img} alt="" />
+                <div>
+                <h2 className="text-2xl">{author}</h2> 
+                <p>{posted_date}</p>
+                </div>
+
+              </div>
+              <div>
+              <span>{reading_time} min read</span>
+              </div>
+            </div>
             <h2 className="text-4xl">{title}</h2> 
+            <p>
+              {
+             hashtags.map(hash => (
+              <span key={blog.id}>
+                <a href="">#{hash} </a>
+              </span> ))
+           
+          }
+            </p>
         </div>
     );
 };
